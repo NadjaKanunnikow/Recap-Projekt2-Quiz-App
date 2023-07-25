ab2695ba565a9ecca9647f7fa0ea93f3b30970f3
Nadja Kanunnikow <nadja.kanunnikow@gmx.de>
2023-07-25 11:28:36 +0200

    add toggle to first bookmark

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // phew… not a lot going on here. Please add some code!
+const toggleBookmarkButton = document.querySelector('[data-js="bookmark"]');
+
+toggleBookmarkButton.addEventListener("click", () => {
+  toggleBookmarkButton.classList.toggle("bookmark--active");
+});
