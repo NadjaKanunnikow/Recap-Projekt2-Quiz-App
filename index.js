d3c99de576448fd358c42dcb9fd5e0013a734a54
Nadja Kanunnikow <nadja.kanunnikow@gmx.de>
2023-07-25 12:26:32 +0200

    add toggle functionality to show answer button

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // phew… not a lot going on here. Please add some code!
-const toggleBookmarkButton = document.querySelector('[data-js="bookmark"]');
+const bookmarkButton = document.querySelector('[data-js="bookmark"]');
+const answerButton = document.querySelector('[data-js="answer-button"]');
+const answer = document.querySelector('[data-ja="answer"]');
 
-toggleBookmarkButton.addEventListener("click", () => {
-  toggleBookmarkButton.classList.toggle("bookmark--active");
+// ADD Toggle functionality to bookmark button:
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("bookmark--active");
+});
+
+// ADD Toggle functionality to answer button:
+answerButton.addEventListener("click", () => {
+  answer.classList.toggle("card__answer");
 });
